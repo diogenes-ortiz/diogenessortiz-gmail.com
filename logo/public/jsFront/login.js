fdda6cad117a432f499684f0a1f9bd5d52b3ab46
Rodrigo Garcia Paz <rodrigogarciapaz@122.fibertel.com.ar>
2020-07-29 13:10:49 -0300

    arregle que se muestren las validaciones de login

--- logo/public/jsFront/login.js
+++ logo/public/jsFront/login.js
@@ -6,17 +6,19 @@ window.addEventListener ('load', function(){
         if (campoEmail.value.length < 2){
             errores.push('Email es obligatorio')
         }
-
+        let contraseña = document.querySelector("label.password")
         let campoContraseña = document.querySelector('input[name=password]')
         if (campoContraseña.value.length < 2){
+            
             errores.push('Contraseña es obligatoria')
         }
         if (errores.length > 0){
             e.preventDefault();
-            let allErrors = document.querySelector('div.errores')
+            let allErrors = document.querySelector('div.col-12.errores')
             for(let i = 0; i < errores.length; i++){
                 allErrors.innerHTML += '<li>' + errores[i] + '</li>'
             }
         }
     })
+
 })
