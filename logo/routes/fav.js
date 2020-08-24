86c9ecb9c1c67f11be41ad8c89d9210a025a7a11
pablistem <pablistem@gmail.com>
2020-08-23 22:41:22 -0300

    CRUD favoritos subsanado

--- logo/routes/fav.js
+++ logo/routes/fav.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var favController = require('../controllers/favController');
 
 /* GET favorite list */
-router.get("/:id", favController.list);
+router.get("/", favController.list);
 
 /* POST favorite add */
-router.post("/:id", favController.add);
+router.post("/", favController.add);
 
 /* DELETE favorite remove */
-router.delete("/:id", favController.remove);
+router.delete("/", favController.remove);
 
 module.exports = router;
